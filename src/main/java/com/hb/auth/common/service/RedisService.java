700b64c3039b7af18e4c1a0673fad34ce6fe5087
Hosni Bounechada <hosni.bounechada@gmail.com>
2023-05-01 16:31:57 +0100

    Implement Redis Caching

---
+++ src/main/java/com/hb/auth/common/service/RedisService.java
@@ -0,0 +1,34 @@
+package com.hb.auth.common.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class RedisService {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    public RedisService(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void setValue(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getValue(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void setValueEx(String key, String value, Duration duration) {
+        redisTemplate.opsForValue().set(key, value, duration);
+    }
+
+    public String getValueEx(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+}
