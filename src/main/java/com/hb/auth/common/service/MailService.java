d2e08f137032ab071b444f521ac8be6c776b87bb
Hosni Bounechada <hosni.bounechada@gmail.com>
2023-05-04 09:57:56 +0100

    Update Mail service Sync and Async

--- src/main/java/com/hb/auth/common/service/MailService.java
+++ src/main/java/com/hb/auth/common/service/MailService.java
@@ -4,6 +4,7 @@ import com.hb.auth.common.template.MailTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,13 @@ import org.springframework.stereotype.Service;
 public class MailService {
     private final JavaMailSender mailSender;
 
-    public void sendMail(SimpleMailMessage simpleMailMessage) {
+    public void sendMailSync(SimpleMailMessage simpleMailMessage) {
+
+        mailSender.send(simpleMailMessage);
+    }
+
+    @Async
+    public void sendMailAsync(SimpleMailMessage simpleMailMessage) {
 
         mailSender.send(simpleMailMessage);
     }
