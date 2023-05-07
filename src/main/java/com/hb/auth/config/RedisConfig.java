b5b44d06137f39fa56b7025b16e1897f24710e92
Hosni Bounechada <hosni.bounechada@gmail.com>
2023-05-07 08:50:33 +0100

    Refactor redis config

--- src/main/java/com/hb/auth/config/RedisConfig.java
+++ src/main/java/com/hb/auth/config/RedisConfig.java
@@ -1,10 +1,14 @@
 package com.hb.auth.config;
 
+import com.hb.auth.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 public class RedisConfig {
